abdb56d6e5652eef7cbfe0ef4f745ab05e73dd0c
Rahul Gudivada <rahulgudivada99@gmail.co>
2016-06-27 22:27:54 -0500

    Final commit for Production

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+
+
+app.set('view engine', 'ejs');
+
+
+var routes = require('./routes');
+
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Routes
+
+//home
+app.get('/', routes.home);
+
+//movie_single
+app.get('/harry_potter_movie/:movie_number?', routes.movie_single);
+	
+// not  found
+app.get('*', routes.notFound);
+
+
+
+
+app.listen(process.env.PORT || 3000);
